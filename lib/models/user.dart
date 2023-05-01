7eb9a1aebd984f26da98984d352f0838cc3f71af
Shubham Daulkar <daulkarshubham@gmail.com>
2023-05-02 02:30:51 +0530

    Post Data SToring in FIrebase

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -22,6 +22,22 @@ class User {
   });
 
 
+  static User fromSnap(DocumentSnapshot snap){
+    var snapshot = snap.data() as Map<String,dynamic>;
+
+  return User(
+    username: snapshot["username"],
+    uid: snapshot["uid"],
+    email: snapshot["email"],
+    photoUrl: snapshot["photoUrl"],
+    bio: snapshot["bio"],
+    followers: snapshot["followers"],
+    following: snapshot["following"],
+    
+  );
+  }
+  
+
   Map<String,dynamic> toJson()=> {
     "username": username,
     "uid": uid,
@@ -31,19 +47,4 @@ class User {
     "followers": followers,
     "following": following,
   };
-
-  static User fromSnap(DocumentSnapshot snap){
-    var snapshot = snap.data() as Map<String,dynamic>;
-
-  return User(
-    username: snapshot['username'],
-    uid: snapshot['uid'],
-    email: snapshot['email'],
-    photoUrl: snapshot['photoUrl'],
-    bio: snapshot['bio'],
-    followers: snapshot['followers'],
-    following: snapshot['following'],
-    
-  );
-  }
 }
